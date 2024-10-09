a7b411a744887b0d7ffd6eda50f0bcb0b10f3933
Armstrong Goes <armstrongmsg@gmail.com>
2024-10-09 00:48:13 -0300

    Set up project

---
+++ src/test/java/com/armstrongmsg/socialnet/mediaservice/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.armstrongmsg.socialnet.mediaservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
